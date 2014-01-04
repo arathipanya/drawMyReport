4467ef862ab0b3c37c65e35cc9ce52c0d6aa7956
root <root@debian.etna-alternance.eu>
2014-01-04 18:44:10 +0100

    bootstrap + google charts + export to image

--- drawMyReport.php
+++ drawMyReport.php
@@ -18,7 +18,7 @@
 	if (!isset ($oreon)) {
 		exit ();
 	}
-	
+
 	#Pear library
 	require_once "HTML/QuickForm.php";
 	require_once 'HTML/QuickForm/advmultiselect.php';
@@ -36,10 +36,122 @@
 	require_once "./include/common/common-Func.php";
 
 	# Smarty template Init
-	$tpl = new Smarty();
+	/*	$tpl = new Smarty();
 	$tpl = initSmartyTpl($path, $tpl);
 $tpl->assign("path", $path);
 	$tpl->assign("contact_infos", getContactInfo());
 	$tpl->assign("drawMyReport_release", "2.0");
 	$tpl->display("drawMyReport.ihtml");
+	*/
+
 ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Bootstrap 101 Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  </head>
+  <body>
+<div id="target">
+
+    <!-- Bootstrap -->
+    <link href="./modules/drawMyReport/include/css/bootstrap.min.css" rel="stylesheet">
+    <script src="http://code.jquery.com/jquery-2.0.0.min.js"></script>
+
+    <script src="./modules/drawMyReport/include/js/bootstrap.min.js"></script>
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+    <script type="text/javascript" src="./modules/drawMyReport/include/js/jsapi.js"></script>
+    <script type="text/javascript">
+
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+      google.setOnLoadCallback(drawChart2);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza I Ate Last Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+
+      function drawChart2() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Mushrooms', 3],
+          ['Onions', 1],
+          ['Olives', 1],
+          ['Zucchini', 1],
+          ['Pepperoni', 2]
+        ]);
+
+        // Set chart options
+        var options = {'title':'How Much Pizza22 I Ate Last22 Night',
+                       'width':400,
+                       'height':300};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div2'));
+        chart.draw(data, options);
+      }
+    </script>
+
+    <script type="text/javascript" src="./modules/drawMyReport/include/js/base.js"></script>
+    <script type="text/javascript" src="./modules/drawMyReport/include/js/html2canvas.js"></script>
+
+
+<script type="text/javascript" src="http://canvg.googlecode.com/svn/trunk/rgbcolor.js"></script> 
+<script type="text/javascript" src="http://canvg.googlecode.com/svn/trunk/StackBlur.js"></script>
+<script type="text/javascript" src="http://canvg.googlecode.com/svn/trunk/canvg.js"></script> 
+
+
+<button type="button" class="btn btn-primary">Primary</button>
+COUCOU release {$dummy_release}<br /><br />
+Hello World, I'm {$contact_infos.contact_name}<br />
+I have installed an Centreon package with a Smarty dummy page, but I can do much more...<br />
+
+<div id="chart_div" class="untarget-after"></div>
+<div id="chart_div2" class="untarget-after"></div>
+
+<canvas id="canvas" class="canvases" width="1" height="1"></canvas>
+<canvas id="canvas2" class="canvases" width="1" height="1"></canvas>
+
+<div class="untarget">
+ <button id="cmd">generate PDF</button>
+</div>
+</div>
+  </body>
+</html>
---
+++ include/js/base.js
@@ -0,0 +1,58 @@
+//include "./html2canvas.js"
+    $(document).ready(function(){ 
+	function cleanHTML() {
+	    var divToClean = $('.untarget');
+	    
+	    for (var i = 0; i < divToClean.length; i++) {
+		console.log(divToClean[i]);
+		divToClean.html("");
+	    }
+	}
+	
+	function convertCharts(canvases, charts) {
+	    var canvases = $(".canvases");
+	    var charts = $('svg');
+	    
+	    for (var i = 0; i < charts.length; i++) {
+		var chart = $('svg:eq(' + i + ')');
+		canvg(canvases[i], chart.parent().html());
+	    }
+	    $(".untarget-after").html("");
+	}
+
+	$('#cmd').click(function () {
+	    cleanHTML();
+	    convertCharts();
+
+//	    canvg(document.getElementById('canvas'), $('svg').parent().html());
+/*
+	    $.ajax({
+	        url: "http://172.16.128.129/centreon/modules/drawMyReport/data/chart2canvas.php",
+	        data: {},
+	        type: 'get',
+	        success: function (result) {
+		    console.log(result);
+		}});
+*/
+	    var capture = {};
+	    var target = $('#target');
+	    html2canvas(target, {
+//		proxy: "http://172.16.128.129/centreon/modules/drawMyReport/include/php/html2canvasproxy.php",
+		onrendered: function(canvas) {
+		    var img = canvas.toDataURL("image/png")
+		    window.open(img);
+		    capture.img = canvas.toDataURL( "image/png" );
+		    capture.data = { 'image' : capture.img };
+                    /* save the image on the server */
+		    $.ajax({
+			url: "http://172.16.128.129/centreon/modules/drawMyReport/include/js/ajax.php",
+			data: capture.data,
+			type: 'post',
+			success: function( result ) {
+			    console.log( result );
+			}
+		    });
+		}
+	    });
+	});
+    });
