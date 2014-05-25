7af13fe8c21f78dbbe23edd653d51f503dd31dbc
Amelie Rathipanya <rathip_a@Rathip-a.local>
2014-05-25 15:45:15 +0200

    ajout de graph

---
+++ graph.php
@@ -0,0 +1,41 @@
+<form class="bs-example bs-example-form" role="form">
+    <div class="input-group">
+      <span class="input-group-addon">Title</span>
+      <input type="text" class="form-control" name="title" placeholder="Title">
+    </div>
+    <br>
+    <div class="input-group">
+      <span class="input-group-addon">Description</span>
+      <input type="text" class="form-control" name="description" placeholder="Description">
+    </div>
+    <br>
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="radio" name="type" value="type1">
+      </span>
+      <span class="input-group-addon">
+        <input type="radio" name="type" value="type2">
+      </span>
+      <span class="input-group-addon">
+        <input type="radio" name="type" value="type3">
+      </span>
+      <span class="input-group-addon">
+        <input type="radio" name="type" value="type4">
+      </span>
+    </div>
+    <br>
+    <div class="input-group">
+      <span class="input-group-addon">
+        <input type="radio" name="period" value="day">
+      </span>
+      <span class="input-group-addon">
+        <input type="radio" name="period" value="week">
+      </span>
+      <span class="input-group-addon">
+        <input type="radio" name="period" value="month">
+      </span>
+      <span class="input-group-addon">
+        <input type="radio" name="period" value="year">
+      </span>
+    </div>
+  </form>
