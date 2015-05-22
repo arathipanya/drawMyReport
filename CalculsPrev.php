3fec464b81ea33566850ac58f57ccd5a426f614d
androidedn <androidedn@gmail.com>
2015-05-22 17:43:35 +0200

    fonction calcul prévisionnel sur un an.

--- CalculsPrev.php
+++ CalculsPrev.php
@@ -40,5 +40,40 @@ static class CalculsPrev
 		$futur_ordo_y = $coef_dir_a * $jour_x + $ordo_b;
 		return $futur_ordo_y;
 	}
+	
+	/*
+	** Cette fonction calcule pour chaque jour sur une période d'un an
+	** les coordonnées en ordonnées des points s'y rapportant.
+	** Un tableau est donc rempli. Il contient les coordonnées des
+	** points d'une courbe prévisionnelle en fonction du coefficient
+	** directeur calculé auparavant.
+	*/
+	public static function calc_futur_ordo_y_for_one_year($coef_dir_a, $jour_x, $ordo_b)
+	{
+		integer $i;
+		$array_ordo_y_one_year = array();
+		
+		$array_ordo_y_one_year = init_array_ordo_y_one_year($array_ordo_y_one_year);
+		for ($i = 0; $i < 364; ++$i)
+		{
+			$array_ordo_y_one_year[$i] = calc_futur_ordo_y($coef_dir_a, $jour_x, $ordo_b);
+		}
+		return $array_ordo_y_one_year;		
+	}
+	
+	/*
+	** Initialisation à zéro de toutes les valeurs du tableau $array_ordo_y_one_year reçu en paramètre.
+	** Renvoie le tableau initialisé.
+	*/
+	private static function init_array_ordo_y_one_year($array_ordo_y_one_year)
+	{
+		integer $i;
+		
+		for ($i = 0; $i < 364; ++$i)
+		{
+			$array_ordo_y_one_year[$i] = 0;			
+		}		
+		return $array_ordo_y_one_year;
+	}
 }
 
